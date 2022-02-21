fe1e47803196ea8b5404a7442649f437f8b76321
kurjakrichard <kurjak.richard@gmail.com>
2022-02-21 10:48:14 +0100

    Switch to local database

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,32 @@
-import 'package:flutter/material.dart';
+import 'package:sqflite_common/sqlite_api.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
-void main() {
-  print('first commit');
+main() {
+  var flutibreMain = FlutibreDatabase();
+  flutibreMain.databasefactory();
+}
+
+class FlutibreDatabase {
+  String path = '/home/sire/VScode/flutibre_pro/assets/books';
+  Future databasefactory() async {
+    // Init ffi loader if needed.
+    sqfliteFfiInit();
+
+    var databaseFactory = databaseFactoryFfi;
+    var db = await databaseFactory.openDatabase('$path/book.db');
+    await db.execute('''
+  CREATE TABLE Product (
+      id INTEGER PRIMARY KEY,
+      title TEXT
+  )
+  ''');
+
+    await db.insert('Product', <String, Object?>{'title': 'Product 1'});
+    await db.insert('Product', <String, Object?>{'title': 'Product 1'});
+
+    var result = await db.query('Product');
+    print(result);
+    // prints [{id: 1, title: Product 1}, {id: 2, title: Product 1}]
+    await db.close();
+  }
 }
